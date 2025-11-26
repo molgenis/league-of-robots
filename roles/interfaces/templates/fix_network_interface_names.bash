3abe1d937f5ec2b4a813136f95c2e1aca3811c0a
Pieter Neerincx <pieter.neerincx@gmail.com>
2025-11-26 10:15:13 +0100

    Various improvements and bug fixes for the interfaces role.

--- roles/interfaces/templates/fix_network_interface_names.bash
+++ roles/interfaces/templates/fix_network_interface_names.bash
@@ -50,23 +50,18 @@ done
 #
 # Apply udev rules to network interfaces sorted by index.
 #
+# When the interface is nevertheless not (re)named to the name reported in the ID_NET_NAME variable:
+# inspect the udevadm output for any rules that output a custom interface NAME variable,
+# which takes precedence over ID_NET_NAME and tweak/disable/modify the corresponding rule.
+#
 for network_interface_index in "${!network_interfaces_sorted[@]}"; do
     if [[ "${udevadm_version%%[^0-9]*}" -ge 250 ]]; then
         id_net_name=$(udevadm test "${network_interfaces_sorted[${network_interface_index}]}" | grep 'ID_NET_NAME=')
-        printf 'INFO: (Re-)applied udev rules for network interface %s with index %d: %s.\n' "${network_interfaces_sorted[${network_interface_index}]}" "${network_interface_index}" "${id_net_name}"
+        printf 'INFO: Applied udev rules for network interface %s with index %d: %s.\n' "${network_interfaces_sorted[${network_interface_index}]}" "${network_interface_index}" "${id_net_name}"
     else
         # Work around for older udevadm on EL 8.x. that does not report ID_NET_NAME.
         id_net_name=$(udevadm test "${network_interfaces_sorted[${network_interface_index}]}")
-        printf 'INFO: (Re-)applied udev rules for network interface %s with index %d.\n' "${network_interfaces_sorted[${network_interface_index}]}" "${network_interface_index}"
-
+        printf 'INFO: Applied udev rules for network interface %s with index %d.\n' "${network_interfaces_sorted[${network_interface_index}]}" "${network_interface_index}"
     fi
 done
-#
-# NetworkManager does not know an interface got renamed;
-# It notices
-#    * the "old" network interface is gone and drops the connection.
-#    * a new network interface appeared and will try to establish a connection.
-# Give NetworkManager some time to configure the new network interface(s) automagically.
-#
-sleep 10
 {% endraw %}
