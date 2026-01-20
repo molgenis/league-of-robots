aacb4d321064efbe94348c4a050a7706ab199ad9
scimerman <sandi.cimerman@gmail.com>
2026-01-20 11:39:20 +0100

    repo_server: added role, scripts

--- roles/repo_server/files/1_sync_repo_to_new_version.sh
+++ roles/repo_server/files/1_sync_repo_to_new_version.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
+set -ueo pipefail
+
 _main="/mnt/repos"
 ! test -d "${_main}/0cache" && mkdir "${_main}/0cache"
 
 print_all_repos() {
-      cd ${_main}/0cache/
-      _allrepos=$(ls -d */ 2>/dev/null)
-      for _thisrepo in ${_allrepos}; do echo "   ${_thisrepo//\/}"; done
+   echo ""
+   echo "All available repositories are"
+   cd ${_main}/dnf.repos.d/
+   find . -name "*.repo" -type f -printf '    %P\n'
 }
 
-if [[ -z "${1}" ]]; then
-      echo "you did not provide repo"
+if [[ -z "${1:-}" ]]; then
+      echo "Error, you did not provide repo!"
       print_all_repos
+      echo ""
+      echo "Correct syntax is"
+      echo "  ${0} os_distribution/repository"
       exit
 fi
 
-if ! test -d ${_main}/0cache/${1}; then
-      echo "this repo does not yet exist in the:"
-      print_all_repos
-      read -n1 -p "> do you want to add this repository as a NEW one? [Y/n]: " _confirm
-      if [[ "${_confirm}" == "" ]] || [[ "${_confirm}" == "Y" ]]; then
-      unset _confirm
-      mkdir "${_main}/0cache/${1}"
-      else
-      exit
-      fi
-fi
+_os_distribution=${1%%/*}
 
-_repo=${1}
+_repo_file=${1##*/}
+_repo=${_repo_file//.repo}
+
+
+echo _os_distribution=$_os_distribution
+echo _repo_file=$_repo_file
+echo _repo=$_repo
+
 _time="$(date +%Y%m%d-%H%M%S)"
-dnf reposync --arch=x86_64 --repo ${_repo} --downloaddir=${_main}/0cache/ && \
-mkdir -p "${_main}/1versions/${_repo}/" && \
-cp -a --reflink=always ${_main}/0cache/${_repo} "${_main}/1versions/${_repo}/${_time}" || exit
+dnf reposync --conf ${_main}/dnf.repos.d/${_os_distribution}.conf --repo ${_repo} --downloaddir=${_main}/0cache/${_os_distribution}/ && \
+mkdir -p "${_main}/1versions/${_os_distribution}/${_repo}/" && \
+cp -a --reflink=always ${_main}/0cache/${_os_distribution}/${_repo}/ "${_main}/1versions/${_os_distribution}/${_repo}/${_time}/" || exit
 echo ""
 echo ""
-echo "New version created:   ${_main}/1versions/${_repo}/${_time}"
+echo "New version created:   ${_main}/1versions/${_os_distribution}/${_repo}/${_time}"
--- roles/repo_server/files/2_new_repo_distribution.sh
+++ roles/repo_server/files/2_new_repo_distribution.sh
@@ -1,68 +1,89 @@
-
-
 #!/bin/bash
+
+set -ueo pipefail
 
 _main="/mnt/repos"
 for _dir in ${_main} ${_main}/1versions ${_main}/2deploy; do
-      test -d ${_dir} || { echo "error no ${_main}"; exit; }
+   test -d ${_dir} || { echo "creating ${_dir}" && mkdir "${_dir}"; }
 done
 
 print_all_stacks() {
-      cd ${_main}/2deploy/
-      echo " available stacks are:"
-      _allstacks=$(ls -d */ 2>/dev/null | grep -v versions)
-      for _thisstack in ${_allstacks}; do echo "   ${_thisstack//\/}"; done
+   cd ${_main}/2deploy/
+   echo "Available stacks are: "
+   find . -mindepth 1 -maxdepth 1 -type d -printf ' %P\n'
 }
 
-if [[ -z "${1}" ]]; then
-      echo "you did not provide stack"
-      print_all_stacks
-      exit
+if [[ -z "${1:-}" ]]; then
+   echo "Error, at least [stack_prefix] is missing. Use either"
+   echo "   ${0} [stack]"
+   echo "or"
+   echo "   ${0} [stack] [OS_distribution] [repository] [version]"
+   print_all_stacks
+   exit
 fi
 
 if ! test -d ${_main}/2deploy/${1}; then
-      echo "this stack not part of the"
-      print_all_stacks
-      read -n1 -p "> do you want to add this stack as NEW stack? [Y/n]: " _confirm
-      if [[ "${_confirm}" == "" ]] || [[ "${_confirm}" == "Y" ]]; then
+   echo "Error: this stack not part of the"
+   print_all_stacks
+   read -n1 -p "> you want to ADD this stack as NEW stack? [Y/n]: " _confirm
+   if [[ "${_confirm}" == "" ]] || [[ "${_confirm}" == "Y" ]] || [[ "${_confirm}" == "y" ]]; then
       unset _confirm
       mkdir "${_main}/2deploy/${1}"
-      else
+   else
       exit
-      fi
+   fi
 fi
 
 _stack=${1}
 
-echo " ________ repositories available __________"
-allrepos=()
-i=0
-cd ${_main}/1versions/
-for _repo in *; do
-      allrepos+=("${_repo}")
-      echo "  ${i}    ${_repo}"
-      i=$((i+1))
-done
-read -p "> select: " reposelect
-echo "  You picked:   ${allrepos[$reposelect]}"
-echo ""
+if [[ "${#}" -eq "4" ]]; then   # if all four variables were provided from command line
+   _distroselect="${2}"
+   _reposelect="${3}"
+   _versionselect="${4}"
+else                            # if not, then prompt for them
+   echo "________ OS distributions available __________"
+   _distroselect="error"
+   while true; do
+      test -d "${_main}/1versions/${_distroselect}" 2>/dev/null && break
+      find "${_main}/1versions/" -mindepth 1 -maxdepth 1 -type d -printf '    %P\n'
+      read -p "> select: " _distroselect
+      echo ""
+      echo "  You picked:   ${_distroselect}"
+      test -d "${_main}/1versions/${_distroselect}" || echo "Wrong distribution, try again!"
+   done
 
-echo " ________ versions available inside ${allrepos[$reposelect]} ____________"
-allversions=()
-j=0
-cd "${_main}/1versions/${allrepos[${reposelect}]}"
+   echo "________ Select repository from the ${_distroselect} __________"
+   _reposelect="error"
+   while true ; do
+      test -d "${_main}/1versions/${_distroselect}/${_reposelect}" 2>/dev/null && break
+      find "${_main}/1versions/${_distroselect}" -mindepth 1 -maxdepth 1 -type d -printf '    %P\n'
+      read -p "> select: " _reposelect
+      echo "  You picked:   ${_reposelect}"
+      test -d "${_main}/1versions/${_distroselect}/${_reposelect}" || echo "Wrong repository, try again!"
+      echo ""
+   done
 
-for _version in *; do
-      allversions+=("${_version}")
-      echo "  ${j}    ${_version}"
-      j=$((j+1))
-done
-read -p "> select: " versionselect
-echo "  You picked:   ${allrepos[$reposelect]}/${allversions[${versionselect}]}"
+   echo "________ Select versions from the ${_distroselect}/${_reposelect} ____________"
+   _versionselect="error"
+   while true; do
+      test -d "${_main}/1versions/${_distroselect}/${_reposelect}/${_versionselect}" 2>/dev/null && break
+      find "${_main}/1versions/${_distroselect}/${_reposelect}" -mindepth 1 -maxdepth 1 -type d -printf '    %P\n'
+      read -p "> select: " _versionselect
+      echo "  You picked:   ${_versionselect}"
+      test -d "${_main}/1versions/${_distroselect}/${_reposelect}/${_versionselect}" || echo "Wrong version, try again!"
+      echo ""
+   done
+fi
 
-echo " ---------- Confirm ---------"
-echo " The link will be created between"
-echo "   2deploy/${_stack}/${allrepos[$reposelect]}  ->  1versions/${allrepos[$reposelect]}/${allversions[versionselect]}"
-read -p "> press [enter] to confirm"
-ln -f -s "${_main}/1versions/${allrepos[$reposelect]}/${allversions[versionselect]}" "${_main}/2deploy/${_stack}/${allrepos[$reposelect]}" && echo done
-echo ""
+cd "${_main}"
+if ! test -d "1versions/${_distroselect}/${_reposelect}/${_versionselect}"; then
+   echo "Error, 1versions/${_distroselect}/${_reposelect}/${_versionselect} does not exist!"
+   exit
+else
+   echo "---------- Confirm ---------"
+   echo " The link will be created between"
+   echo "   2deploy/${_stack}/${_reposelect}  ->  1versions/${_distroselect}/${_reposelect}/${_versionselect}"
+   read -p "> press [enter] to confirm"
+   ln -f -s "1versions/${_distroselect}/${_reposelect}/${_versionselect}" "2deploy/${_stack}/${_reposelect}"
+   echo ""
+fi
